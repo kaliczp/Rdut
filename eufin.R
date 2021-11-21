6ddcb363f965f624268f1b761c8bf3e5135b234c
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-11-21 08:16:23 +0100

    Conversion to numeric with apply
    Simpler conversion with apply

--- eufin.R
+++ eufin.R
@@ -5,7 +5,7 @@ eufin <- function(file) {
     raw.nofirst.mat <- matrix(raw.nofirst, ncol = 17, byrow = TRUE)
     raw.nofirst.mat.csak <- raw.nofirst.mat[,c(3,5,7,9,11,13,15,17)]
     char.mat <- sub(",", ".", raw.nofirst.mat.csak)
-    data.mat <- matrix(as.numeric(char.mat), nrow = nrow(char.mat))
+    data.mat <- apply(char.mat, 2, as.numeric)
     colnames(data.mat) <- c("Sz_ért", "T", "B", "SZ_B", "SZ_T", "Felesleg", "Hiány", "Plusz")
     data.mat
 }
