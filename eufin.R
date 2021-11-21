05d67462686d6bb32730cdd52db96fa9ecb335ec
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-11-21 08:01:54 +0100

    Data matrix created
    Data matrix without first row

--- eufin.R
+++ eufin.R
@@ -4,5 +4,8 @@ eufin <- function(file) {
     raw.nofirst <- raw[-(1:7)]
     raw.nofirst.mat <- matrix(raw.nofirst, ncol = 17, byrow = TRUE)
     raw.nofirst.mat.csak <- raw.nofirst.mat[,c(3,5,7,9,11,13,15,17)]
-    sub(",",".",raw.nofirst.mat.csak)
+    char.mat <- sub(",", ".", raw.nofirst.mat.csak)
+    data.mat <- matrix(as.numeric(char.mat), nrow = nrow(char.mat))
+    colnames(data.mat) <- c("Sz_ért", "T", "B", "SZ_B", "SZ_T", "Felesleg", "Hiány", "Plusz")
+    data.mat
 }
