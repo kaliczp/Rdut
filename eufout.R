d18236718b9dc5af36f2ac6aa1082744d00ad785
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-11-22 05:59:17 +0100

    More accurate output with tags
    Now the fileld descriptions are created in the export function.

--- eufout.R
+++ eufout.R
@@ -1,3 +1,22 @@
 eufout <- function(x, file.name = "Rout.euf") {
-    write(x, file.name)
+    c("Sz_ért", "T", "B", "SZ_B", "SZ_T", "Felesleg", "Hiány", "Plusz")
+    full.mat <- cbind(Ini = "[Sz]",
+                      Szé = "<Sz_ért>",
+                      x[,1],
+                      T = "<T>",
+                      x[,2],
+                      B = "<B>",
+                      x[,3],
+                      SZB = "<SZ_B>",
+                      x[,4],
+                      SZT = "<SZ_T>",
+                      x[,5],
+                      Fleg = "<Felesleg>",
+                      x[,6],
+                      Hi = "<Hiány>",
+                      x[,7],
+                      Pl = "<Plusz>",
+                      x[,8])
+    asc.out <- sub("\\.", ",", full.mat)
+    write(t(asc.out), file.name)
 }
