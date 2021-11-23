36e0007460912740220672c106940e8e75aba686
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-11-23 05:17:21 +0100

    Crossession ID and data extracted from dxf
    Dxf extraction is made by grep in shell. Extracted data are imported and processed.

--- processing.R
+++ processing.R
@@ -1,2 +1,12 @@
 test <- eufin("test.euf")
 eufout(test)
+
+## grep "[BT]:" FoldtomOri.dxf > btori.txt
+## grep "[0-9]+[0-9]" FoldtomOri.dxf > szelvori.txt
+## grep "[BT]:" FoldJavb23.dxf > btjav.txt
+## grep "[0-9]+[0-9]" FoldJavb23.dxf > szelv.txt
+
+szelv <- scan("szelv.txt", character(), sep = "\n")
+btjav <- scan("btjav.txt", character(), sep = "\n")
+
+cbind(szelv, matrix(btjav, ncol =2, byrow = TRUE))
